d1d0251775f3515620b895ad7ad6df0424e303b8
Joel Edwards <joeledwards@gmail.com>
2014-11-05 07:18:50 -0700

    Initial work on app body using (very) simple reactjs components

--- www/scripts.js
+++ www/scripts.js
@@ -1,8 +1,77 @@
+var menus = [
+    {
+        title : "Home",
+        path : "home"
+    },
+    {
+        title : "Our Mission",
+        path : "mission"
+    },
+    {
+        title : "Media",
+        path : "media"
+    },
+    {
+        title : "Kenya",
+        path : "kenya"
+    },
+    {
+        title : "Faculty & Staff",
+        path : "staff"
+    },
+    {
+        title : "Join Our Team",
+        path : "team"
+    },
+    {
+        title : "Contact Us",
+        path : "contact"
+    }
+];
+
 var App = React.createClass({
     render: function() {
-        console.log("Blah!");
         return (
-            <h1>Hello, world!</h1>
+        <div>
+            <Menu />
+            <Content />
+        </div>
+        );
+    }
+});
+
+var Menu = React.createClass({
+    render: function() {
+        return (
+        <div name="menu">
+            <ul>{
+                menus.map(function(item, index) {
+                    return <MenuItem title={item.title} path={'#' + item.path} />;
+                })
+            }</ul>
+        </div>
+        );
+    }
+});
+
+var MenuItem = React.createClass({
+    render: function() {
+        return (
+        <li>
+            <a href={this.path}>{this.title}</a>
+        </li>
+        )
+    }
+});
+
+var Content = React.createClass(
+{
+    render: function()
+    {
+        return (
+        <div name="content">
+            Nothing to see here. Move along.
+        </div>
         );
     }
 });
