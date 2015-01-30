cad92ef878cc7222f3a0fe7f8a8432e56219bd79
Joel Edwards <joeledwards@gmail.com>
2015-01-29 21:12:22 -0700

    Updated formatting of server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var node_static = require('node-static');
 var file_server = new node_static.Server('www');
-var server = http.createServer(function(request, response)
-{
-    request.addListener('end', function()
-    {
+
+http.createServer(function(request, response) {
+    request.addListener('end', function() {
         file_server.serve(request, response);
-    }).resume();
-});
-server.listen(8080);
+    })
+    .resume();
+})
+.listen(8080);
+
